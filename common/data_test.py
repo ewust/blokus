f40390002a5199a0a4a41fde0c7b4a72c794ef44
Nate Slottow <nslottow@gmail.com>
2012-09-09 12:35:28 -0400

    Added initial unit tests for data.  Fleshed out some data methods.  Added global test runner (test.py) that runs all test cases in all files that match *_test.py.  Note that tests must be written as if they reside in the top-level directory

---
+++ common/data_test.py
@@ -0,0 +1,82 @@
+﻿import unittest
+
+from common.data import *
+        
+class BlockTests(unittest.TestCase):
+    def test_create(self):
+        block = Block(0, 0)
+        self.assertEqual(block.piece_id, 0)
+        self.assertEqual(block.player_id, 0)
+        self.assertFalse(block.is_empty)
+    
+    def test_empty(self):
+        block = Block(EMPTY_PIECE_ID, 0)
+        self.assertTrue(block.is_empty)
+        block = Block(0, EMPTY_PLAYER_ID)
+        self.assertTrue(block.is_empty)
+        
+class PieceTests(unittest.TestCase):
+    def test_create(self):
+        pass
+        
+    def test_create_from_string(self):
+        text = """
+.O.
+.OO
+OO.
+"""
+
+        piece = Piece.from_string(0, text)
+        self.assertEqual(str(piece).strip(),
+"""
+id=0
+.O.
+.OO
+OO.
+""".strip())
+    
+    def test_to_string(self):
+        piece = Piece(0, [ Point(0, 0), Point(1, 0), Point(1, 1), Point(1, 2) ])
+        self.assertEqual(str(piece).strip(),
+"""
+id=0
+.O
+.O
+OO
+""".strip())
+    
+    def test_get_rotation(self):
+        pass
+        
+    def test_get_rotation_steps(self):
+        pass
+        
+    def test_is_rotation(self):
+        pass
+        
+class BoardTests(unittest.TestCase):
+    _pieces_text = [
+        """
+        .O.
+        OOO
+        ...
+        """,
+        """
+        .O.
+        OO.
+        .O.
+        """,
+        """
+        ...
+        OOO
+        .O.
+        """,
+        """
+        .O.
+        .OO
+        .O.
+        """ ]
+
+    def test_block_data(self):
+        pass
+        
