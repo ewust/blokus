493f235be814fc3eb0f5833009a426c7fa813b59
Pat Pannuto <pat.pannuto@gmail.com>
2012-09-13 05:32:21 -0400

    dummybot: Make dumber again, don't need corner optimization w/ Board fix

--- client/bots/dummybot.py
+++ client/bots/dummybot.py
@@ -4,10 +4,10 @@ from copy import copy
 
 from common.data import Move
 from common.data import Point
-from common.bot import SimpleStatusHandler,CornerLoggingBot
+from common.bot import SimpleStatusHandler,PlayOnReport,Bot
 
 """Dumbest bot that can play the game"""
-class DummyBot(SimpleStatusHandler, CornerLoggingBot):
+class DummyBot(SimpleStatusHandler, PlayOnReport, Bot):
     """Initializes the bot for a new game"""
     def __init__(self, **kwds):
         super(DummyBot, self).__init__(**kwds)
@@ -20,22 +20,12 @@ class DummyBot(SimpleStatusHandler, CornerLoggingBot):
         if self.have_skipped:
             return Move.skip(self.player_id)
 
-        for c in self.corner_set:
-            for piece in self.remaining_pieces:
-                for rotation in xrange(4):
-                    move = Move(self.player_id, piece, rotation, c)
-                    if self.board.is_valid_move(move):
-                        self.remaining_pieces.remove(piece)
-                        return move
-
         for piece in self.remaining_pieces:
             for rotation in xrange(4):
                 for x in xrange(self.board.size):
                     for y in xrange(self.board.size):
                         move = Move(self.player_id, piece, rotation, (x,y))
                         if self.board.is_valid_move(move):
-                            print "Legal move: " + str(move)
-                            print "Not in corner set: " + str(self.corner_set)
                             self.remaining_pieces.remove(piece)
                             return move
 
